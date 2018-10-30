3b7e6566519fb45515af267c36b67259108df1d5
HeavenVolkoff <vasconcellos.dev@gmail.com>
2018-10-30 18:21:38 -0300

    Version 0.0.2  - Commented out some deprecate fields  - Filled out some metadatas fields

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# find_namespace_packages requires setuptools>=40.1.0
 from setuptools import setup, find_namespace_packages
 
 setup(packages=find_namespace_packages("src"), package_dir={"": "src"})
