857f29f32266674bd3c89ee8bcfcc5f4a2900841
Vítor Vasconcellos <HeavenVolkoff@users.noreply.github.com>
2020-12-18 16:36:51 -0300

    Restore incorrectly removed licenses  - Fix logic in setup_license.sh to account for more changes in the format  - Simplify logic now that there is a single jsonId file with all license texts

--- tools/setup_license.sh
+++ tools/setup_license.sh
@@ -6,19 +6,7 @@ set -u          # don't allow not set variables to be utilized
 set -o pipefail # trace ERR through pipes
 set -o errtrace # trace ERR through 'time command' and other functions
 
-curl -LSs https://github.com/spdx/license-list-data/archive/master.zip | bsdtar -xf - --strip-components 1 license-list-data-master/jsonld
-trap '[ -d jsonld ] && rm -r jsonld' INT TERM EXIT
-IF="if"
-for file in jsonld/*; do
-    [ "${file: -7}" == ".jsonld" ] || continue
-    license=$(basename "$file" .jsonld)
-    text="$(jq -r '.["@graph"][1] | select((.isFsfLibre == "true" or .isOsiApproved == "true") and (.isDeprecatedLicenseId == null or .isDeprecatedLicenseId == "false")) | .licenseText' "${file}")"
-    [ -z "$text" ] && continue
-    echo >&2 "$license"
-    cat <<EOF
-{% ${IF} cookiecutter.project_license == "${license}" -%}
-${text}
-EOF
-    IF="elif"
-done
+echo "{% if cookiecutter.project_license == \"NOT_USED\" -%}"
+curl -LSs https://github.com/spdx/license-list-data/raw/master/jsonld/licenses.jsonld |
+    jq -r '.["@graph"] | map(select((.isFsfLibre == "true" or .isOsiApproved == "true") and (.isDeprecatedLicenseId == null or .isDeprecatedLicenseId == "false"))) | .[] | "{% elif cookiecutter.project_license == \"\(.licenseId)\" -%}\n\(.licenseText)"'
 echo "{% endif -%}"
